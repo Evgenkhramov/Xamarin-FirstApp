6a9df72a2aa6c07099174dab9a40725b43bd54cd
Yvgen Khramov <Evgenkhramov@gmail.com>
2019-02-21 15:35:27 +0200

    Add project files.

---
+++ TipCalc.Droid/Views/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using TipCalc.Core.ViewModels;
+using TipCalc.Droid;
+
+namespace CalcApp.UI.Droid
+{
+    [Activity(Label = "My Tip Calc", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true, Icon = "@mipmap/ic_launcher")]
+    public class SplashScreenActivity : MvxSplashScreenActivity
+    {
+        public SplashScreenActivity()
+            : base(Resource.Layout.SplashScreen)
+        {
+        }
+    }
+}
